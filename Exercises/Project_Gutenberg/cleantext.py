8842414dc838c3c9e7892d5b95ab55550924fe8f
Sergei Merson <sergei.merson@gmail.com>
2019-11-26 09:14:19 +0200

    Finished Progect_Gutenberg assignment

--- Exercises/Project_Gutenberg/cleantext.py
+++ Exercises/Project_Gutenberg/cleantext.py
@@ -4,28 +4,28 @@ def get_stopwords(stopwords_path):
 
 
 def get_non_chars():
-    non_chars = {chr(i) for i in range(32,65)}
-    non_chars.update({chr(i) for i in range(91, 97)})
-    non_chars.update({chr(i) for i in range(123, 128)})
-    # non_chars.discard('-')
-    return ''.join(non_chars)
+    ascii_non_chars = list(range(0,65)) + list(range(91,97)) + list(range(123,128))
+    ascii_val_chars = list(range(65,91)) + list(range(97,123))
+    non_chars = (chr(i) for i in ascii_non_chars)
+    val_chars = {chr(i) for i in ascii_val_chars}
+    return ''.join(non_chars), val_chars
 
 
-def clear_non_chars(word, non_chars):
+def clear_non_chars(word, non_chars, val_chars):
     if not word.isalpha():
-        word = ''.join([letter for letter in word if letter not in non_chars])
+        word = word.strip(non_chars)
+        word = word[:-2] if word[-2:] in ('’s','’d') else word
+        word = word[:-3] if word[-3:] in ('’st','’d') else word
+        word = ''.join([letter for letter in word if letter in val_chars])
         return word
 
 
-def process_line(line:str, non_chars:str, stopwords:set, vocabulary:dict):
-    # Split, convert to lowercase and strip:
-    words = [word.strip(non_chars).lower() for word in line.split()]
+def acceptable_word(word, stopwords):
+    return (word is not None) and (word not in stopwords) and (len(word) > 0)
 
-    # Iterate over each word:
-    for word in words:
-        #word = clear_non_chars(word, non_chars)
-        # Append only words that not in stopwords:
-        if (word not in stopwords) and (word is not None):
-            vocabulary[word] = vocabulary.get(word, 0) + 1
 
-    return vocabulary
+def split_line(line, delimiters):
+    for d in delimiters:
+        line = line.replace(' '+d, ' ')
+    list_of_words = line.split()
+    return list_of_words
--- sketches.py
+++ sketches.py
@@ -1,35 +1,44 @@
 from Exercises.Project_Gutenberg import cleantext
 
 
-# Get file from disk:
-book_path = r"d:\Projects\ExperisAcademy\Exercises\Project_Gutenberg\Macbeth.txt"
-stopwords_path = r"d:\Projects\ExperisAcademy\Exercises\Project_Gutenberg\stopwords.txt"
+def process_line(line: str, non_chars: str, val_chars, stopwords: str, vocabulary: dict):
+    # Split and convert to lowercase:
+    list_of_words = cleantext.split_line(line, ['—'])
+
+    # Iterate over each word:
+    for word in list_of_words:
+        word = word.lower()
+        word = cleantext.clear_non_chars(word, non_chars, val_chars)
+        # Append only words that not in stopwords:
+        if cleantext.acceptable_word(word, stopwords):
+            vocabulary[word] = vocabulary.get(word, 0) + 1
+
+    return vocabulary
 
 
 def count_words(book_path, stopwords_path):
-
     stopwords = cleantext.get_stopwords(stopwords_path)
-    non_chars = cleantext.get_non_chars()
+    non_chars, val_chars = cleantext.get_non_chars()
     vocabulary = {}
 
     with open(book_path, 'r', encoding='UTF-8') as book_file:
         current_line = book_file.readline()
 
         while current_line:
-            vocabulary = cleantext.process_line(current_line, non_chars, stopwords, vocabulary)
+            vocabulary = process_line(current_line, non_chars, val_chars, stopwords, vocabulary)
             current_line = book_file.readline()
-
-    # # Reverse vocabulary:
-    # result = {val: key for key, val in vocabulary.items()}
 
     # Print result in descending order:
     for key, val in sorted(vocabulary.items(),
-                      key=lambda key_val: (key_val[1], (key_val[0])),
-                      reverse=True):
+                           key=lambda key_val: (key_val[1], (key_val[0])),
+                           reverse=True):
         print(f'{key:<15s} {val:<4d}', sep='\t')
 
     return vocabulary
 
 
 if __name__ == '__main__':
+    # Get file from disk:
+    book_path = r"d:\Projects\ExperisAcademy\Exercises\Project_Gutenberg\Macbeth.txt"
+    stopwords_path = r"d:\Projects\ExperisAcademy\Exercises\Project_Gutenberg\stopwords.txt"
     count_words(book_path, stopwords_path)
